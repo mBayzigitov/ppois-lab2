fd00a4c6cc4baedf14b62fdeeb90d9d73390b422
Murat Bayzigitov <bayzigitov.murat@gmail.com>
2025-02-25 15:38:41 +0300

    updated script

--- nw_setup.py
+++ nw_setup.py
@@ -1,60 +1,83 @@
 from mininet.net import Mininet
-from mininet.node import Controller, RemoteController, OVSKernelSwitch, Node
+from mininet.node import Controller, RemoteController, OVSSwitch, Node
+from mininet.link import TCLink
 from mininet.cli import CLI
 from mininet.log import setLogLevel
 
+def configure_routing(router, routes):
+    """ Configure routing table for a router """
+    for route, via in routes.items():
+        router.cmd(f'ip route add {route} via {via}')
+
+def enable_ip_forwarding(router):
+    """ Enable IP forwarding for routers """
+    router.cmd("sysctl -w net.ipv4.ip_forward=1")
+
 def setup_network():
-    net = Mininet(controller=Controller, switch=OVSKernelSwitch)
+    net = Mininet(controller=Controller, link=TCLink)
+
+    print("*** Adding controller")
+    net.addController("c0")
 
     print("*** Creating nodes")
-    r1 = net.addHost('r1', ip='10.0.1.1/24')
-    r2 = net.addHost('r2', ip='10.0.2.1/24')
+    h1 = net.addHost("h1", ip="10.0.1.2/24")
+    h2 = net.addHost("h2", ip="10.0.1.3/24")
+    h3 = net.addHost("h3", ip="10.0.2.2/24")
+    server = net.addHost("server", ip="10.0.2.3/24")
 
-    h1 = net.addHost('h1', ip='10.0.1.10/24')
-    h2 = net.addHost('h2', ip='10.0.1.11/24')
-    h3 = net.addHost('h3', ip='10.0.2.10/24')
-    server = net.addHost('server', ip='10.0.2.20/24')
+    r1 = net.addHost("r1")  # Router 1
+    r2 = net.addHost("r2")  # Router 2
 
     print("*** Creating switches")
-    s1 = net.addSwitch('s1')
-    s2 = net.addSwitch('s2')
+    s1 = net.addSwitch("s1")
+    s2 = net.addSwitch("s2")
 
     print("*** Creating links")
     net.addLink(h1, s1)
     net.addLink(h2, s1)
-    net.addLink(r1, s1)
-    net.addLink(r1, r2)
-    net.addLink(r2, s2)
+    net.addLink(s1, r1)  # r1-eth1 connects to s1
+
+    net.addLink(r1, r2)  # r1-eth2 <-> r2-eth1 (direct router link)
+
+    net.addLink(r2, s2)  # r2-eth2 connects to s2
     net.addLink(h3, s2)
     net.addLink(server, s2)
 
     print("*** Starting network")
     net.start()
 
-    print("*** Configuring routers")
-    r1.cmd('sysctl -w net.ipv4.ip_forward=1')
-    r2.cmd('sysctl -w net.ipv4.ip_forward=1')
+    print("*** Configuring IP addresses")
+    # Router 1 interfaces
+    r1.cmd("ifconfig r1-eth0 10.0.1.1/24 up")   # LAN side (to h1, h2 via s1)
+    r1.cmd("ifconfig r1-eth1 192.168.1.1/30 up")  # Link to Router 2
 
-    print("*** Setting up IP routes")
-    r1.cmd('ip addr add 10.0.1.1/24 dev r1-eth1')
-    r1.cmd('ip addr add 192.168.1.1/30 dev r1-eth2')
-    r2.cmd('ip addr add 192.168.1.2/30 dev r2-eth1')
-    r2.cmd('ip addr add 10.0.2.1/24 dev r2-eth2')
+    # Router 2 interfaces
+    r2.cmd("ifconfig r2-eth0 192.168.1.2/30 up")  # Link to Router 1
+    r2.cmd("ifconfig r2-eth1 10.0.2.1/24 up")   # LAN side (to h3, server via s2)
 
-    r1.cmd('ip route add 10.0.2.0/24 via 192.168.1.2 dev r1-eth2')
-    r2.cmd('ip route add 10.0.1.0/24 via 192.168.1.1 dev r2-eth1')
+    print("*** Enabling IP forwarding")
+    enable_ip_forwarding(r1)
+    enable_ip_forwarding(r2)
 
-    h1.cmd('ip route add default via 10.0.1.1')
-    h2.cmd('ip route add default via 10.0.1.1')
-    h3.cmd('ip route add default via 10.0.2.1')
-    server.cmd('ip route add default via 10.0.2.1')
+    print("*** Configuring static routing")
+    configure_routing(r1, {"10.0.2.0/24": "192.168.1.2"})  # Route to h3, server via r2
+    configure_routing(r2, {"10.0.1.0/24": "192.168.1.1"})  # Route to h1, h2 via r1
 
-    print("*** Running CLI")
+    print("*** Setting default gateways for hosts")
+    h1.cmd("ip route add default via 10.0.1.1")
+    h2.cmd("ip route add default via 10.0.1.1")
+    h3.cmd("ip route add default via 10.0.2.1")
+    server.cmd("ip route add default via 10.0.2.1")
+
+    print("*** Testing connectivity")
+    net.pingAll()
+
+    print("*** Starting CLI")
     CLI(net)
 
     print("*** Stopping network")
     net.stop()
 
-if __name__ == '__main__':
-    setLogLevel('info')
+if __name__ == "__main__":
+    setLogLevel("info")
     setup_network()
