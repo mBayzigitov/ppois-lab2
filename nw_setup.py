9400ba2ad7984ccf23b0ede0086f2a19061d23d4
Murat Bayzigitov <bayzigitov.murat@gmail.com>
2025-03-04 12:53:22 +0300

    from scratch

--- nw_setup.py
+++ nw_setup.py
@@ -1,112 +1,43 @@
+from mininet.topo import Topo
 from mininet.net import Mininet
-from mininet.node import Controller, RemoteController, OVSSwitch, Node
-from mininet.link import TCLink
+from mininet.node import Controller, OVSKernelSwitch, RemoteController
 from mininet.cli import CLI
-from mininet.log import setLogLevel
-from time import sleep
+from mininet.link import TCLink
 
-def configure_routing(router, routes):
-    """ Configure routing table for a router """
-    for route, via in routes.items():
-        router.cmd(f'ip route add {route} via {via}')
+class QoSTopo(Topo):
+    def build(self):
+        # Создаём маршрутизаторы
+        r1 = self.addSwitch('s1')
+        r2 = self.addSwitch('s2')
 
-def enable_ip_forwarding(router):
-    """ Enable IP forwarding for routers """
-    router.cmd("sysctl -w net.ipv4.ip_forward=1")
+        # Добавляем хосты (клиенты)
+        h1 = self.addHost('h1', ip='192.168.1.1/24')
+        h2 = self.addHost('h2', ip='192.168.1.2/24')
+        h3 = self.addHost('h3', ip='192.168.1.3/24')
 
-def setup_qos(router):
-    for iface in router.intfNames():
-        router.cmd(f"tc qdisc add dev {iface} root handle 1: htb default 30")
-        router.cmd(f"tc class add dev {iface} parent 1: classid 1:1 htb rate 1000kbps ceil 1000kbps")
-        router.cmd(f"tc class add dev {iface} parent 1: classid 1:2 htb rate 512kbps ceil 512kbps")
-        router.cmd(f"tc class add dev {iface} parent 1: classid 1:3 htb rate 256kbps ceil 256kbps")
+        # Добавляем сервер
+        server = self.addHost('server', ip='192.168.2.1/24')
 
-        router.cmd(f"tc qdisc add dev {iface} parent 1:1 handle 10: police rate 1000kbps burst 10kb mpu 64k")
-        router.cmd(f"tc qdisc add dev {iface} parent 1:2 handle 20: police rate 512kbps burst 10kb mpu 64k")
-        router.cmd(f"tc qdisc add dev {iface} parent 1:3 handle 30: police rate 256kbps burst 10kb mpu 64k")
+        # Соединяем маршрутизаторы
+        self.addLink(r1, r2, bw=10)  # Пропускная способность 10 Мбит/с
 
-        router.cmd(f"tc filter add dev {iface} protocol ip prio 1 u32 match ip sport 5060 0xffff flowid 1:1")
-        router.cmd(f"tc filter add dev {iface} protocol ip prio 2 u32 match ip sport 554 0xffff flowid 1:2")
-        router.cmd(f"tc filter add dev {iface} protocol ip prio 3 u32 match ip sport 80 0xffff flowid 1:3")
-    print(f"QoS policies applied to {router.name}")
+        # Подключаем хосты к r1
+        self.addLink(h1, r1, bw=1)  # 1 Мбит/с
+        self.addLink(h2, r1, bw=1)
+        self.addLink(h3, r1, bw=1)
 
-def generate_traffic(h1, h2, h3, server):
-    print("*** Generating traffic")
-    server.cmd("iperf -s -u -p 5060 &")  # Voice traffic
-    server.cmd("iperf -s -u -p 554 &")  # Video traffic
-    server.cmd("iperf -s -u -p 80 &")   # HTTP traffic
-    sleep(1)
-    h1.cmd("iperf -c 10.0.2.3 -u -p 5060 -b 800k &")  # Voice
-    h2.cmd("iperf -c 10.0.2.3 -u -p 554 -b 400k &")  # Video
-    h3.cmd("iperf -c 10.0.2.3 -u -p 80 -b 200k &")  # HTTP
+        # Подключаем сервер к r2
+        self.addLink(server, r2, bw=10)  # 10 Мбит/с
 
-def setup_network():
-    net = Mininet(controller=Controller, link=TCLink)
 
-    print("*** Adding controller")
-    net.addController("c0")
-
-    print("*** Creating nodes")
-    h1 = net.addHost("h1", ip="10.0.1.2/24")
-    h2 = net.addHost("h2", ip="10.0.1.3/24")
-    h3 = net.addHost("h3", ip="10.0.2.2/24")
-    server = net.addHost("server", ip="10.0.2.3/24")
-
-    r1 = net.addHost("r1")  # Router 1
-    r2 = net.addHost("r2")  # Router 2
-
-    print("*** Creating switches")
-    s1 = net.addSwitch("s1")
-    s2 = net.addSwitch("s2")
-
-    print("*** Creating links")
-    net.addLink(h1, s1)
-    net.addLink(h2, s1)
-    net.addLink(s1, r1)
-    net.addLink(r1, r2)
-    net.addLink(r2, s2)
-    net.addLink(h3, s2)
-    net.addLink(server, s2)
-
-    print("*** Starting network")
+# Запуск Mininet с топологией
+if __name__ == '__main__':
+    topo = QoSTopo()
+    net = Mininet(topo=topo, controller=Controller, switch=OVSKernelSwitch, link=TCLink)
     net.start()
 
-    print("*** Configuring IP addresses")
-    r1.cmd("ifconfig r1-eth0 10.0.1.1/24 up")
-    r1.cmd("ifconfig r1-eth1 192.168.1.1/30 up")
-    r2.cmd("ifconfig r2-eth0 192.168.1.2/30 up")
-    r2.cmd("ifconfig r2-eth1 10.0.2.1/24 up")
-
-    print("*** Enabling IP forwarding")
-    enable_ip_forwarding(r1)
-    enable_ip_forwarding(r2)
-
-    print("*** Configuring static routing")
-    configure_routing(r1, {"10.0.2.0/24": "192.168.1.2"})
-    configure_routing(r2, {"10.0.1.0/24": "192.168.1.1"})
-
-    print("*** Setting default gateways for hosts")
-    h1.cmd("ip route add default via 10.0.1.1")
-    h2.cmd("ip route add default via 10.0.1.1")
-    h3.cmd("ip route add default via 10.0.2.1")
-    server.cmd("ip route add default via 10.0.2.1")
-
-    print("*** Applying QoS policies")
-    setup_qos(r1)
-    setup_qos(r2)
-
-    print("*** Testing connectivity")
+    print("Сеть запущена. Проверяем соединения:")
     net.pingAll()
 
-    print("*** Generating traffic")
-    generate_traffic(h1, h2, h3, server)
-
-    print("*** Starting CLI")
-    CLI(net)
-
-    print("*** Stopping network")
+    CLI(net)  # Открываем CLI Mininet
     net.stop()
-
-if __name__ == "__main__":
-    setLogLevel("info")
-    setup_network()
