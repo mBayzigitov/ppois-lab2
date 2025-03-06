db9d2ab563f54959c1c3b54b67ccb12c204ca989
Murat Bayzigitov <bayzigitov.murat@gmail.com>
2025-03-06 22:45:52 +0300

    meh

--- classifier.py
+++ classifier.py
@@ -3,7 +3,6 @@ import ipaddress
 
 from scapy.all import sniff, IP, UDP, TCP
 
-
 def classify_and_mark(packet):
     if not packet.haslayer(IP):
         return
@@ -17,20 +16,19 @@ def classify_and_mark(packet):
 
     traffic_type = "data"
     if packet.haslayer(UDP):
-        match ip_layer.tos:
-            case 184:
-                traffic_type = "voice"
-            case 160:
-                traffic_type = "video"
+        if ip_layer.tos == 184:
+            traffic_type = "voice"
+        elif ip_layer.tos == 160:
+            traffic_type = "video"
     elif packet.haslayer(TCP):
         traffic_type = "data"
         
-    print(f"Пакет {ip_layer.src} -> {ip_layer.dst}: Тип - {traffic_type}, TOS - {ip_layer.tos}")
+    print(f"Packer {ip_layer.src} -> {ip_layer.dst}: Type - {traffic_type}, TOS - {ip_layer.tos}")
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         sys.exit(1)
 
     interface = sys.argv[1]
-    print(f"Запуск классификатора на интерфейсе {interface}")
+    print(f"Classificator is running on the interface {interface}")
     sniff(iface=interface, prn=classify_and_mark, store=0)
